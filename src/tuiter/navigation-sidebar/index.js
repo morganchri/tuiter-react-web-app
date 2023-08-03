02dbd9d48a9b3b21d7ad585f1800e682016e3e12
morganchri <morgan.chri@northeastern.edu>
2023-08-03 18:26:50 -0400

    a5 REST su23

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -8,14 +8,14 @@ const NavigationSidebar = () => {
     const { currentUser } = useSelector((state) => state.user);
     const { pathname } = useLocation();
     const [ignore, tuiter, active] = pathname.split("/");
-    const links = ["home",     "explore",   "notifications", "messages", "bookmarks", "lists", "profile",  "more"];
+    const links = ["home",     "explore",   "notifications", "messages", "bookmarks", "lists",  "more"];
     const icons = [<i className="fa fa-home"></i>,
                    <i className="fa fa-hashtag wd-icon-buffer"></i>,
                    <i className="fa fa-bell wd-icon-buffer"></i>,
                    <i className="fa fa-envelope wd-icon-buffer"></i>,
                    <i className="fa fa-bookmark wd-icon-buffer"></i>,
                    <i className="fa fa-list wd-icon-buffer"></i>,
-                   <i className="fa fa-user wd-icon-buffer"></i>,
+                   // <i className="fa fa-user wd-icon-buffer"></i>,
                    <i className="fas fa-circle wd-icon-buffer"></i>]
 
     // https://stackoverflow.com/questions/32937181/javascript-es6-map-multiple-arrays
