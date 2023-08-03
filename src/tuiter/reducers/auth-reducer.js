84aebfc336df5c6732474926199fae1502510f79
morganchri <morgan.chri@northeastern.edu>
2023-08-02 20:15:46 -0400

    a5 REST su23

--- src/tuiter/reducers/auth-reducer.js
+++ src/tuiter/reducers/auth-reducer.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { loginThunk, logoutThunk, profileThunk, updateUserThunk, register} from "../services/auth-thunks";
+import { loginThunk, logoutThunk, profileThunk, updateUserThunk, registerThunk} from "../services/auth-thunks";
 
 
 const authSlice = createSlice({
