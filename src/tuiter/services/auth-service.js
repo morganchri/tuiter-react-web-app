11d2e59e746b808507b621e6d152b54ff8bd251c
morganchri <morgan.chri@northeastern.edu>
2023-08-03 15:30:29 -0400

    a5 REST su23

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -2,6 +2,8 @@ import axios from "axios";
 // import "dotenv/config";
 
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+// const SERVER_API_URL = ["https://tuiter-node-server-app-cs5610-s232.onrender.com/api", 
+						// "http://localhost:4000/api"]
 // const SERVER_API_URL = process.env.REACT_APP_API_BASE;
 const USERS_URL = `${SERVER_API_URL}/users`;
 
