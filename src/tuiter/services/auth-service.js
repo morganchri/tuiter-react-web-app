4598ba03ec8be563442cf5b897fb673f8bf48a12
morganchri <morgan.chri@northeastern.edu>
2023-08-03 00:20:08 -0400

    a5 REST su23

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
 const USERS_URL = `${SERVER_API_URL}/users`;
 
+console.log("server api url");
 console.log(SERVER_API_URL);
 
 const api = axios.create({ withCredentials: true });
